8ed9c73897cd450bf16cc29f4aea4de7297920ad
Tran Sang <tranvansangk41@gmail.com>
2019-11-26 17:12:31 +0900

    init commit

---
+++ index.ts
@@ -0,0 +1,114 @@
+import AWS from 'aws-sdk'
+import path from 'path'
+import ms from 'ms'
+import fs from 'fs'
+import StorageBase from 'ghost-storage-base'
+import {Handler} from 'express'
+import {promisify} from 'util'
+
+interface IS3GhostConfig {
+	accessKeyId: string
+	secretAccessKey: string
+	bucketName: string
+	region: string
+	assetsBaseUrl: string
+}
+
+const serverPath = '../../../../core/server'
+
+module.exports = class S3Ghost extends StorageBase {
+	private readonly s3Instance: AWS.S3
+	private readonly options: IS3GhostConfig
+	private readonly storagePath: string
+
+	constructor(config: IS3GhostConfig) {
+		super()
+		this.storagePath = require(`${serverPath}/config`).getContentPath('images')
+		this.options = config
+		const awsConfig = new AWS.Config({
+			accessKeyId: config.accessKeyId,
+			secretAccessKey: config.secretAccessKey,
+			region: config.region
+		})
+		awsConfig.setPromisesDependency(Promise)
+		this.s3Instance = new AWS.S3({
+			apiVersion: '2006-03-01',
+			params: {Bucket: config.bucketName},
+			accessKeyId: config.accessKeyId,
+			secretAccessKey: config.secretAccessKey,
+			region: config.region
+		})
+		this.s3Instance.config = awsConfig
+	}
+
+	getAWSKey(targetFileName: string){
+		return path.relative(this.storagePath, targetFileName)
+	}
+
+	async exists(fileName: string, targetDir?: string): Promise<boolean> {
+		targetDir = targetDir || super.getTargetDir(this.storagePath)
+		const targetFileName = path.join(targetDir, fileName)
+		try {
+			await this.s3Instance.headObject({
+				Key: this.getAWSKey(targetFileName),
+				Bucket: this.options.bucketName
+			}).promise()
+			return true
+		} catch (e) {
+			return false
+		}
+	}
+
+	async save(image: StorageBase.Image, targetDir?: string): Promise<string> {
+		targetDir = targetDir || super.getTargetDir(this.storagePath)
+		const targetFileName = await super.getUniqueFileName(image, targetDir)
+		await this.s3Instance.putObject({
+			ContentType: image.type,
+			Key: this.getAWSKey(targetFileName),
+			Body: await promisify(fs.readFile.bind(fs))(image.path),
+			Bucket: this.options.bucketName
+		}).promise()
+		const urlUtils = require(`${serverPath}/lib/url-utils`)
+		return urlUtils.urlJoin(
+				'/',
+				urlUtils.getSubdir(),
+				urlUtils.STATIC_IMAGE_URL_PREFIX,
+				path.relative(this.storagePath, targetFileName)
+		).replace(new RegExp(`\\${path.sep}`, 'g'), '/')
+	}
+
+	serve(): Handler {
+		return (req, res) => {
+			res
+					.status(301)
+					.redirect(`${this.options.assetsBaseUrl}${req.url.replace(/\/$/, '')}`)
+		}
+	}
+
+	async delete(fileName: string, targetDir?: string): Promise<boolean> {
+		targetDir = targetDir || super.getTargetDir(this.storagePath)
+		const targetFileName = path.join(targetDir, fileName)
+		try {
+			await this.s3Instance.deleteObject({
+				Key: this.getAWSKey(targetFileName),
+				Bucket: this.options.bucketName
+			}).promise()
+			return true
+		} catch (e) {
+			return false
+		}
+	}
+
+	async read(options?: StorageBase.ReadOptions): Promise<Buffer> {
+		const targetPath = path.join(
+				this.storagePath,
+				// remove trailing slashes
+				(options && options.path || '').replace(/\/$|\\$/, '')
+		)
+		const response = await this.s3Instance.getObject({
+			Key: targetPath,
+			Bucket: this.options.bucketName
+		}).promise()
+		return response.Body as Buffer
+	}
+}
---
+++ webpack.config.ts
@@ -0,0 +1,8 @@
+export default {
+	entry: './index.js',
+	devtool: 'source-map',
+	output: {
+		path: __dirname,
+		filename: 's3_ghost.js',
+	},
+}
